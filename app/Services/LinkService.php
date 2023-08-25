3bc9238ea37ef871081d70a80b5a8bd4eb1783b7
raquelizek <raquelreclizek@gmail.com>
2023-08-24 22:35:33 -0300

    feat: modifying functions in the link service

--- app/Services/LinkService.php
+++ app/Services/LinkService.php
@@ -2,6 +2,7 @@
 
 namespace App\Services;
 
+use App\Models\Link;
 use App\Repositories\LinkRepositoryInterface;
 use Exception;
 
@@ -20,10 +21,21 @@ class LinkService
             $alreadyExists = $this->linkRepositoryInterface->getLink($data["link"]);
 
             if (!empty($alreadyExists)) {
-                throw new Exception('O QR Code deste link já foi criado.', 400);
+                throw new Exception('Este link já foi criado.', 400);
             }
 
-            $this->linkRepositoryInterface->store($data);
+            if (empty($data["identifier"])) {
+                $uniqueIdentifier = $this->generateUniqueIdentifier();
+                $data["identifier"] = $uniqueIdentifier;
+            }
+
+            $shortenedUrl = url("/visit/{$data["identifier"]}");
+
+            $this->linkRepositoryInterface->store([
+                "link" => $data["link"],
+                "identifier" => $data["identifier"],
+                "encurted_link" => $shortenedUrl,
+            ]);
 
             return response()->json(
                 [
@@ -38,9 +50,44 @@ class LinkService
                     "type" => "error",
                     "message" => $exception->getMessage(),
                 ],
+                $exception->getCode()
             );
         }
 
+    }
+
+    private function generateUniqueIdentifier($length = 8)
+    {
+        try {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyz';
+            $randomString = '';
+            for ($i = 0; $i < $length; $i++) {
+                $randomString .= $characters[rand(0, strlen($characters) - 1)];
+            }
+
+            $randomString = str_replace(' ', '', $randomString);
+
+            $existingIdentifiers = Link::pluck('identifier')->toArray();
+            while (in_array($randomString, $existingIdentifiers)) {
+                $randomString = '';
+                for ($i = 0; $i < $length; $i++) {
+                    $randomString .= $characters[rand(0, strlen($characters) - 1)];
+                }
+                $randomString = str_replace(' ', '', $randomString);
+            }
+
+            return $randomString;
+
+        } catch (\Exception $exception) {
+
+            return response()->json(
+                [
+                    "type" => "error",
+                    "message" => $exception->getMessage(),
+                ],
+                $exception->getCode()
+            );
+        }
     }
 
     public function getList()
@@ -48,26 +95,49 @@ class LinkService
         try {
             $data = $this->linkRepositoryInterface->getList();
             return $data;
+
         } catch (\Exception $exception) {
+
             return response()->json(
                 [
                     "type" => "error",
                     "message" => $exception->getMessage(),
                 ],
+                $exception->getCode()
             );
         }
     }
 
-    public function get($id)
+    public function get($identifier)
     {
         try {
-            return $this->linkRepositoryInterface->get($id);
+            return $this->linkRepositoryInterface->get($identifier);
+
         } catch (\Exception $exception) {
+
             return response()->json(
                 [
                     "type" => "error",
                     "message" => $exception->getMessage(),
                 ],
+                $exception->getCode()
+            );
+        }
+    }
+
+    public function getById($identifier)
+    {
+        try {
+            return $this->linkRepositoryInterface->getById($identifier);
+
+        } catch (\Exception $exception) {
+
+            return response()->json(
+                [
+                    "type" => "error",
+                    "message" => $exception->getMessage(),
+                ],
+                $exception->getCode()
             );
         }
     }
@@ -76,12 +146,14 @@ class LinkService
     {
         try {
             return $this->linkRepositoryInterface->update($data, $id);
+
         } catch (\Exception $exception) {
             return response()->json(
                 [
                     "type" => "error",
                     "message" => $exception->getMessage(),
                 ],
+                $exception->getCode()
             );
         }
     }
@@ -96,12 +168,14 @@ class LinkService
                     "message" => 'Excluído com sucesso!',
                 ],
             );
+
         } catch (\Exception $exception) {
             return response()->json(
                 [
                     "type" => "error",
                     "message" => $exception->getMessage(),
                 ],
+                $exception->getCode()
             );
         }
     }
