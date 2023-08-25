08a87e3fb76bc617148ef454cb70dddc2886cc4c
raquelizek <raquelreclizek@gmail.com>
2023-08-24 22:36:04 -0300

    feat: organizing and adding new routes

--- routes/api.php
+++ routes/api.php
@@ -1,20 +1,49 @@
 <?php
 
+use App\Http\Controllers\AccessController;
 use App\Http\Controllers\LinkController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/store', [LinkController::class, 'store']);
+Route::prefix('link')->group(function () {
 
-Route::get('/get-list', [LinkController::class, 'getList']);
+    Route::post('/store', [LinkController::class, 'store']);
 
-Route::get('/get/{id}', [LinkController::class, 'get']);
+    Route::get('/list', [LinkController::class, 'all']);
 
-Route::patch(
-    '/update/{id}',
-    [LinkController::class, 'update']
-);
+    Route::get('/{id}', [LinkController::class, 'getById']);
 
-Route::delete(
-    '/delete/{id}',
-    [LinkController::class, 'destroy']
-);
+    Route::get('/get/{identifier}', [LinkController::class, 'get']);
+
+    Route::patch(
+        '/update/{id}',
+        [LinkController::class, 'update']
+    );
+
+    Route::delete(
+        '/delete/{id}',
+        [LinkController::class, 'destroy']
+    );
+
+});
+
+Route::prefix('access')->group(function () {
+
+    Route::post('/store', [AccessController::class, 'store']);
+
+    Route::get('/list', [AccessController::class, 'all']);
+
+    Route::get('/get/{id}', [AccessController::class, 'get']);
+
+    Route::get('/', [AccessController::class, 'getAccess']);
+
+    Route::patch(
+        '/update/{id}',
+        [AccessController::class, 'update']
+    );
+
+    Route::delete(
+        '/delete/{id}',
+        [AccessController::class, 'destroy']
+    );
+
+});
